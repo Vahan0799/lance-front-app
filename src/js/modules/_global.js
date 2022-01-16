c6dbd57df1dc6df638617f154695c91b3f30894e
Vahan <vahan.baghdasaryan.99@gmail.com>
2022-01-16 22:32:16 +0400

    Finished Demo

--- src/js/modules/_global.js
+++ src/js/modules/_global.js
@@ -1,58 +1,102 @@
 import $ from 'jquery';
-import LazyLoad from 'vanilla-lazyload';
-import 'jquery.cookie';
-import { clickOutSide } from './_utils';
-
 
 export default class Global {
-
     constructor() {
         this.init();
     }
 
     init() {
-        this.initLazyLoad();
-        this.cookieBanner();
-        this.toggleNav();
+        this.toggleSidebar();
+        this.outSideClose();
+        this.showPassword();
+        this.toggleModalContent();
+        this.mobNavPanel();
     }
 
-    initLazyLoad() {
-        new LazyLoad();
+    // Sidebar menu
+    toggleSidebar() {
+        let body = document.querySelector('body');
+        $('.nav__list--open').on('click', () => {
+            $('.nav__list').addClass('nav__list--active');
+            body.style.overflow = 'hidden';
+        })
+        $('.nav__list--close').on('click', () => {
+            $('.nav__list').removeClass('nav__list--active');
+            body.style.overflow = '';
+        })
     }
 
-    cookieBanner() {
-        if (!$.cookie('cookie-banner')) {
-            if (!$('.cookie-banner').is(':visible')) {
-                $('.cookie-banner').removeClass('d-none');
-                this.initLazyLoad();
-            } else {
-                $('.newsletter-banner').addClass('d-none');
-            }
+    // Close modal on outside click
+    outSideClose() {
+        const modal = $('.modal');
+        const openModal = $('.web__login');
+        const closeModal = $('.modal__close');
+        const body = document.querySelector('body');
+
+        openModal.on('click', showModal);
+        closeModal.on('click', hideModal);
+
+        $(window).on('click', event => {
+            if (!modal.hasClass('modal--active')) return
+            if (!event.target.closest('.modal__container')) hideModal()
+        })
+
+        function showModal(event) {
+            event.stopPropagation();
+            modal.addClass('modal--active');
+            body.style.overflow = 'hidden';
         }
 
-        $('.cookie-banner__close').each(function() {
-            $(this).click(function() {
-                $(this).parents('.cookie-banner').fadeOut(200);
-                $.cookie('cookie-banner', true, { path: '/', expires: 7 });
-            });
-        });
+        function hideModal(event) {
+            modal.removeClass('modal--active');
+            $('.modal__container form').each(function () {
+                $(this)[0].reset();
+            })
+            body.style.overflow = '';
+            $('.register').hide();
+            $('.login').show();
+        }
     }
 
-    toggleNav() {
-        this.navbarNav = $('.navbar__nav');
-        this.navTogglerBtn = $('.navbar__toggler');
-        this.search = $('.search');
+    // Change modal form content
+    toggleModalContent() {
+        $('.create-account').on('click', () => {
+            $('.login').hide();
+            $('.register').show();
+        })
+    }
 
-        // click out-side dropdown
-        clickOutSide((dropdown) => $(dropdown).removeClass('dropdown--open'), '.dropdown', '.navbar__nav-link');
+    // Show password characters
+    showPassword() {
+        let show = document.querySelectorAll('.show-hide-password');
 
-        $(this.navTogglerBtn).on('click', () => {
-            if ($(window).width() <= 991.98) {
-                $(this.navbarNav).fadeToggle(200);
-                $(this.navTogglerBtn).toggleClass('navbar__toggler--open');
-                $(this.search).fadeToggle(200);
-                $('body').toggleClass('nav-visible');
-            }
-        });
+        show.forEach((e) => {
+            e.addEventListener('click', () => {
+                const modalInput = e.closest('.modal__input');
+                const inputPass = modalInput.querySelector('.password');
+                const showPassword = modalInput.querySelector('.show-password');
+                const hidePassword = modalInput.querySelector('.hide-password');
+
+                if (inputPass.type === 'password') {
+                    inputPass.type = 'text';
+                    showPassword.style.display = 'none';
+                    hidePassword.style.display = 'block';
+                } else {
+                    inputPass.type = 'password';
+                    showPassword.style.display = 'block';
+                    hidePassword.style.display = 'none';
+                }
+            })
+        })
+    }
+
+    // Navigation Panel for Mobile
+    mobNavPanel() {
+        if ($(window).width() <= 992) {
+            $('.web__navbar--tab--wrapper').first().on('click', function (e) {
+                e.preventDefault()
+                $(this).toggleClass('tab--mob--active')
+            })
+        }
     }
 }
